e07be5294ef538dbc7d3bc15c80048b6fb66fde4
willstrach <willstrach@gmail.com>
2017-01-18 21:51:18 +0000

    The begginings of a corpus analyser has been implemented. At this point it only finds and activates word senses.

--- wnDisambiguator.py
+++ wnDisambiguator.py
@@ -1,1 +1,35 @@
 from memory import *
+from nltk.corpus import brown as corpus
+from nltk.corpus import wordnet as wn
+from sys import stdout
+
+
+#initialise memory structures
+episodicBuffer = episodicBuffer(1, 1)
+stm = stm(5, 0, 1, 1)
+memoryController = memoryController(stm, episodicBuffer)
+
+# format testing data as a list of documents
+test_files = corpus.fileids()
+total_corpus = []
+for f in test_files:
+    total_corpus.append(corpus.sents(f))
+
+
+def corpus_analyser(input_corpus):
+    counter = 0
+    for sentence in input_corpus:
+        counter = counter + 1
+        print counter
+        for word in sentence:
+            word_analyser(word)
+
+    for item in stm.getContents():
+        print str(item.getSynset()) + ", " + str(item.getActivation)
+
+def word_analyser(input_word):
+    word_senses = wn.synsets(input_word)
+    for sense in word_senses:
+        memoryController.activateSynset(sense)
+
+corpus_analyser(total_corpus[0])
